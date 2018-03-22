a5b123227c5ae87541e732b780a4b8cc0f3b3f14
Michal Beran <beran.mi.g@gmail.com>
2018-03-22 17:08:35 +0100

    Added show details on click in list of tests. (#20)

--- TestMeApp/app/src/main/java/cz/muni/fi/pv239/testmeapp/model/Test.java
+++ TestMeApp/app/src/main/java/cz/muni/fi/pv239/testmeapp/model/Test.java
@@ -2,12 +2,16 @@ package cz.muni.fi.pv239.testmeapp.model;
 
 import io.realm.RealmList;
 import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+import io.realm.annotations.Required;
 
 /**
  * Created by Michal on 21.03.2018.
  */
 
 public class Test extends RealmObject{
+    @Required
+    @PrimaryKey
     public String url;
     public String name;
     public Integer testCount;
