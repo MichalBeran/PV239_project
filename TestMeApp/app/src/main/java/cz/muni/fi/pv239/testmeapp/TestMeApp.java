bd911712ac18a539c9ee572ecf40a368d3e36b32
MichalBeran <beran.michal@mail.muni.cz>
2018-03-25 21:52:04 +0200

    API 26 fix.

--- TestMeApp/app/src/main/java/cz/muni/fi/pv239/testmeapp/TestMeApp.java
+++ TestMeApp/app/src/main/java/cz/muni/fi/pv239/testmeapp/TestMeApp.java
@@ -3,7 +3,11 @@ package cz.muni.fi.pv239.testmeapp;
 import android.app.Application;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Build;
 import android.preference.PreferenceManager;
+import android.util.DisplayMetrics;
 
 import java.util.Locale;
 
@@ -14,7 +18,7 @@ import io.realm.Realm;
 public class TestMeApp
         extends Application {
 
-    public static Context context;
+    public static Context appContext;
     private static TestMeApp sInstance;
     public static final String LANGUAGE_PREFERENCES = "pref_selected_language";
     private static SharedPreferences sharedPreferences;
@@ -26,14 +30,14 @@ public class TestMeApp
     public void onCreate() {
         super.onCreate();
         sInstance = this;
-        context = getApplicationContext();
+        appContext = getApplicationContext();
 
         Realm.init(this);
 
         PreferenceManager.setDefaultValues(this, R.xml.settings, false);
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         String language = getLang();
-        changeLang(context, language);
+        changeLang(appContext, language);
     }
 
     public static void changeLang(Context context, String lang) {
@@ -41,7 +45,25 @@ public class TestMeApp
         Locale.setDefault(myLocale);
         android.content.res.Configuration config = new android.content.res.Configuration();
         config.locale = myLocale;
-        context.getResources().updateConfiguration(config, context.getResources().getDisplayMetrics());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            context.createConfigurationContext(config);
+        }else {
+            context.getResources().updateConfiguration(config, context.getResources().getDisplayMetrics());
+        }
+//        Resources resources = context.getResources();
+//        Configuration configuration = resources.getConfiguration();
+//        android.content.res.Configuration configuration = new android.content.res.Configuration();
+//        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1){
+//            configuration.setLocale(myLocale);
+//        } else{
+//            configuration.locale=myLocale;
+//        }
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
+//            context.createConfigurationContext(configuration);
+//        } else {
+//            context.getResources().updateConfiguration(configuration,displayMetrics);
+//        }
     }
 
     public static String getLang(){
