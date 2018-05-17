765ae32fb59dbe6db1d428783e0fdc021722d82e
MichalBeran <beran.michal@mail.muni.cz>
2018-05-17 15:53:51 +0200

    Added downloading test dialog.

--- TestMeApp/app/src/main/java/cz/muni/fi/pv239/testmeapp/activity/GetTestActivity.java
+++ TestMeApp/app/src/main/java/cz/muni/fi/pv239/testmeapp/activity/GetTestActivity.java
@@ -1,14 +1,22 @@
 package cz.muni.fi.pv239.testmeapp.activity;
 
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Message;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -54,7 +62,51 @@ public class GetTestActivity extends AppCompatActivity{
     }
 
     private void loadTest(@NonNull final String testname) {
-        Call<Test> testCall = mTestApi.getService().getTest(testname);
+        final ProgressDialog mProgressDialog = new ProgressDialog(this);
+        mProgressDialog.setIndeterminate(true);
+        mProgressDialog.setMessage(getString(R.string.test_downloading));
+        mProgressDialog.setCancelable(true);
+        mProgressDialog.setCanceledOnTouchOutside(true);
+
+        final Dialog mDialog;
+        final Dialog mSuccessDialog;
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        mDialog = builder.setTitle(R.string.test_download_failed)
+                .setPositiveButton(R.string.retry, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        loadTest(testname);
+                    }
+                })
+                .setNegativeButton(R.string.text_cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .create();
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(this);
+        mSuccessDialog = mBuilder.setTitle(R.string.test_save_successful)
+                .setPositiveButton(R.string.button_ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .create();
+
+        final Call<Test> testCall = mTestApi.getService().getTest(testname);
+
+        mProgressDialog.setButton(DialogInterface.BUTTON_NEGATIVE, getString(R.string.text_cancel), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                testCall.cancel();
+                dialog.dismiss();
+            }
+        });
+        mProgressDialog.show();
+
         testCall.enqueue(new Callback<Test>() {
 
             @Override
@@ -65,20 +117,37 @@ public class GetTestActivity extends AppCompatActivity{
                 }
                 test.url = mTestApi.getUrlBase() + testname;
                 saveResult(test);
+                if(mProgressDialog.isShowing()){
+                    mProgressDialog.dismiss();
+                    mSuccessDialog.show();
+                    final Timer t = new Timer();
+                    t.schedule(new TimerTask() {
+                        public void run() {
+                            if(mSuccessDialog.isShowing()) {
+                                mSuccessDialog.dismiss();
+                            }
+                            t.cancel();
+                        }
+                    }, 3000);
+                }
             }
 
             @Override
             public void onFailure(Call<Test> call, Throwable t) {
                 t.printStackTrace();
-                Snackbar.make(findViewById(R.id.getTestLayout), R.string.test_download_failed, Snackbar.LENGTH_LONG)
-                        .setAction(R.string.retry, new View.OnClickListener() {
-                            @Override
-                            public void onClick(View v) {
-                                // Respond to the click, such as by undoing the modification that caused
-                                // this message to be displayed
-                                loadTest(testname);
-                            }
-                        }).show();
+//                Snackbar.make(findViewById(R.id.getTestLayout), R.string.test_download_failed, Snackbar.LENGTH_LONG)
+//                        .setAction(R.string.retry, new View.OnClickListener() {
+//                            @Override
+//                            public void onClick(View v) {
+//                                // Respond to the click, such as by undoing the modification that caused
+//                                // this message to be displayed
+//                                loadTest(testname);
+//                            }
+//                        }).show();
+                if(mProgressDialog.isShowing()){
+                    mProgressDialog.dismiss();
+                    mDialog.show();
+                }
             }
         });
     }
@@ -96,8 +165,9 @@ public class GetTestActivity extends AppCompatActivity{
         mRealm.close();
     }
 
-    private void saveResult(final Test test) {
+    private Boolean saveResult(final Test test) {
         Realm realm = null;
+        Boolean state = false;
         try {
             realm = Realm.getDefaultInstance();
             realm.executeTransaction(new Realm.Transaction() {
@@ -106,12 +176,14 @@ public class GetTestActivity extends AppCompatActivity{
                     realm.insertOrUpdate(test);
                 }
             });
-            Snackbar.make(findViewById(R.id.getTestLayout), R.string.test_save_successful, Snackbar.LENGTH_LONG).show();
+//            Snackbar.make(findViewById(R.id.getTestLayout), R.string.test_save_successful, Snackbar.LENGTH_LONG).show();
+            state = true;
         } finally {
             if(realm != null) {
                 realm.close();
             }
         }
+        return state;
     }
 
     @OnClick(R.id.scanButton)
