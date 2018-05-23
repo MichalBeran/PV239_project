c9fcf3ce303577443e19a592f08ee93cc8811e34
František Hrdina <franta.hrdina@mail.muni.cz>
2018-05-23 09:57:00 +0200

    Implemented downloading of tests from a list
    User can view the list of test, and choose which he can downloads

---
+++ TestMeApp/app/src/main/java/cz/muni/fi/pv239/testmeapp/model/TestLight.java
@@ -0,0 +1,10 @@
+package cz.muni.fi.pv239.testmeapp.model;
+
+/**
+ * Created by Franta on 23.05.2018.
+ */
+
+public class TestLight {
+    // Name from serialized ouput of api
+    public String name;
+}
